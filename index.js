09e43cf88cb2aef11c846b690149ad228ba1e229
Aakashdeep954 <aakashdeep954@gmail.com>
2021-07-10 23:19:31 +0530

    add start script

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/',(req, res)=>{
     res.render('home.ejs')
 })
 
-app.listen(3000,()=>{
-    console.log("listening on port 3000")
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
 })
